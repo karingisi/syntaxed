25fdd68a96d011449de3fb07241f82a8cba107ad
karingisi <silvanus.matiku@gmail.com>
2015-06-05 11:07:20 +1000

    Added Theming

--- components/Syntaxed.php
+++ components/Syntaxed.php
@@ -10,21 +10,55 @@ class Syntaxed extends ComponentBase
     {
         return [
             'name'        => 'Syntaxed Component',
-            'description' => 'Highlights Code when added on the page'
+            'description' => 'Highlights Code when added on the page using PrismJS'
         ];
     }
 
     //No properties to define on the component form
     public function defineProperties()
     {
-        return [];
+        return [
+             'theme' => [
+                'description' => 'Theme',
+                'title'       => 'Theme',
+                'default'     => 'default',
+                'type'        => 'dropdown',
+                'options'     => ['default'=>'Default','dark'=>'Dark','funky'=>'Funky','okaidia'=>'Okaidia','twilight'=>'Twilight','coy'=>'Coy']
+            ]
+        ];
     }
 
     //Initial run event.
     public function onRun()
     {
-        //add css and JS
-        $this->addCss('/plugins/matiku/syntaxed/assets/css/prism.css');
-        $this->addJs('/plugins/matiku/syntaxed/assets/js/prism.js');  
+        //add css depending on theme selected
+        $theme = $this->property('theme');
+
+        switch ($theme) {
+            case "dark":
+                $this->addCss('/plugins/matiku/syntaxed/assets/css/prism_dark.css');
+                break;
+
+            case "funky":
+                $this->addCss('/plugins/matiku/syntaxed/assets/css/prism_funky.css'); 
+                break;
+
+            case "okaidia":
+                $this->addCss('/plugins/matiku/syntaxed/assets/css/prism_okaidia.css'); 
+                break;
+
+            case "twilight":
+                $this->addCss('/plugins/matiku/syntaxed/assets/css/prism_twilight.css'); 
+                break;  
+
+            case "coy":
+                $this->addCss('/plugins/matiku/syntaxed/assets/css/prism_coy.css'); 
+                break;  
+
+            default:
+                $this->addCss('/plugins/matiku/syntaxed/assets/css/prism.css'); 
+        }
+        //Add JS
+        $this->addJs('/plugins/matiku/syntaxed/assets/js/prism.js');
     }
 }
